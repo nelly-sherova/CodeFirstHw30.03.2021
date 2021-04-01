0caaed3aa6e90b8ad9ff55d0b38236461f8e6acd
unknown <nellysherova@mail.ru>
2021-04-01 12:17:37 +0500

    Почти готовый проект, отсуствует лишь миграция и view

---
+++ codeFirstHW/Db/SampleData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace codeFirstHW.Db
+{
+    public class SampleData
+    {
+    }
+}
---
+++ codeFirstHW/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace codeFirstHW.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
